f5be2ba523077409e3cbe358e3795deead88bbdc
RubenVG02 <rubenvillegas2@gmail.com>
2024-07-14 12:45:53 +0200

    edit

--- src/enrichment.py
+++ src/enrichment.py
@@ -8,58 +8,69 @@ import glob
 
 def select_files():
     '''
-    Function to select the candidates, universe and background files for the enrichment analysis. All files must contain the same ID format.
+    Function to select the candidates, universe, and background files for the enrichment analysis. 
+    All files must contain the same ID format.
 
     Returns:
 
-    - candidates: List with the candidate transcripts 
-    - universe: List with the universe transcripts 
+    - grouped_files: Dictionary with basenames as keys and sub-dictionaries as values. 
+      Each sub-dictionary contains paths for 'candidates' and 'universe' files.
     - background: DataFrame with the background transcripts 
     '''
-    candidates = []
-    universe = []
+    grouped_files = {}
     background = pd.DataFrame()
 
     # Ensure the 'data' directory exists
     if not os.path.exists('data'):
         os.makedirs('data')
 
-    # Check if there are files in the data folder
-    if os.path.exists('data'):
-        # Get the files in the data folder
-        files = os.listdir('data')
-        if len(files) != 0:
-            candidate_files = glob.glob('data/*candidates.txt')
-            universe_files = glob.glob('data/*universe.txt')
-            annotation_files = glob.glob('data/*.annotation*')
+    if os.path.exists('data') and len(os.listdir('data')) != 0:
+        candidate_files = glob.glob('data/candidates/*.candidates.txt')
+        universe_files = glob.glob('data/universe/*.universe.txt')
+        annotation_files = glob.glob('data/annotation/*.annotation*')
 
-            if candidate_files and universe_files and annotation_files:
-                candidates = [line.strip() for line in open(candidate_files[0])]
-                universe = [line.strip() for line in open(universe_files[0])]
-                process_eggnog(annotation_files[0], 'data/background.txt')
-                background = pd.read_csv('data/background.txt', sep='\t', header=None, names=['GO', 'Evidence', 'Transcript'])
-                
-                return candidates, universe, background
+        if candidate_files and universe_files and annotation_files:
+            for file in candidate_files:
+                basename = os.path.basename(file).replace('.candidates.txt', '')
+                if basename not in grouped_files:
+                    grouped_files[basename] = {'candidates': [], 'universe': []}
+                grouped_files[basename]['candidates'].append(file)
+            
+            for file in universe_files:
+                basename = os.path.basename(file).replace('.universe.txt', '')
+                if basename not in grouped_files:
+                    grouped_files[basename] = {'candidates': [], 'universe': []}
+                grouped_files[basename]['universe'].append(file)
+
+            # Process the annotation file
+            process_eggnog(annotation_files[0], 'data/background.txt')
+            background = pd.read_csv('data/background.txt', sep='\t', header=None, names=['GO', 'Evidence', 'Transcript'])
+            
+            return grouped_files, background
+        
 
     # If the files are not found, open file dialog for user to select the files
     root = Tk()
-    root.withdraw()  # Hide the root window
+    root.withdraw()  
+
+    group_file = {}
 
     candidate_file = filedialog.askopenfilename(title="Select candidates file", filetypes=[("Text files", "*.txt")])
     if candidate_file:
         shutil.copy(candidate_file, 'data/')
-        candidates = [line.strip() for line in open(candidate_file)]
+
 
     universe_file = filedialog.askopenfilename(title="Select universe file", filetypes=[("Text files", "*.txt")])
     if universe_file:
         shutil.copy(universe_file, 'data/')
-        universe = [line.strip() for line in open(universe_file)]
 
     annotation_file = filedialog.askopenfilename(title="Select annotation file", filetypes=[("Annotation files", "*.annotation")])
     if annotation_file:
         shutil.copy(annotation_file, 'data/') #Copy all used files to the data folder
         process_eggnog(annotation_file, 'data/background.txt')
         background = pd.read_csv('data/background.txt', sep='\t', header=None, names=['GO', 'Evidence', 'Transcript'])
+
+    grouped_files["selected"]= 
 
     return candidates, universe, background
 
